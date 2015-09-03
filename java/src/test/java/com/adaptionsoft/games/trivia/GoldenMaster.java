b1b8698136b78aba099082b7a036a0e002a7cf2b
Franziska Sauerwein <franzi@codurance.com>
2015-09-03 13:36:24 +0100

    introduce private methods to make test more readable

--- java/src/test/java/com/adaptionsoft/games/trivia/GoldenMaster.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GoldenMaster.java
@@ -10,34 +10,50 @@ import org.junit.Test;
 public class GoldenMaster {
     @Test
     public void run_golden_master() throws FileNotFoundException {
-        System.setOut(new PrintStream(new FileOutputStream("master.txt")));
+        writeOutputTo("master.txt");
 
-        for (int seed = 0; seed < 500; seed++) {
+        runGameTimes(500);
+    }
+
+    private void writeOutputTo(String filename) throws FileNotFoundException {
+        System.setOut(new PrintStream(new FileOutputStream(filename)));
+    }
+
+    private void runGameTimes(int numberOfGamesRun) {
+        for (int seed = 0; seed < numberOfGamesRun; seed++) {
             Random rand = new Random(seed);
             runOneGame(rand);
         }
     }
 
     private void runOneGame(Random rand) {
+        Game aGame = createGame();
+
+        boolean notAWinner;
+        do {
+            aGame.executeMove(rand.nextInt(5) + 1);
+            notAWinner = checkIfWinner(answerWasWrong(rand), aGame);
+
+        } while (notAWinner);
+    }
+
+    private boolean checkIfWinner(boolean answerWasWrong, Game aGame) {
+        if (answerWasWrong)
+            return aGame.movePlayerInPenaltyBoxAfterWrongAnswerAndMoveOnToNextPlayer();
+        return aGame.determineIfPlayerWonAfterCorrectAnswerAndMoveOnToNextPlayer();
+    }
+
+    private boolean answerWasWrong(Random rand) {
+        return rand.nextInt(9) == 7;
+    }
+
+    private Game createGame() {
         Game aGame = new Game();
 
         aGame.add("Chet");
         aGame.add("Pat");
         aGame.add("Sue");
 
-
-        boolean notAWinner;
-        do {
-
-            aGame.executeMove(rand.nextInt(5) + 1);
-
-            if (rand.nextInt(9) == 7) {
-                notAWinner = aGame.movePlayerInPenaltyBoxAfterWrongAnswerAndMoveOnToNextPlayer();
-            } else {
-                notAWinner = aGame.determineIfPlayerWonAfterCorrectAnswerAndMoveOnToNextPlayer();
-            }
-
-
-        } while (notAWinner);
+        return aGame;
     }
 }

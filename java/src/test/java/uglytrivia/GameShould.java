9270b851beca3e04126d4a78dc10a93321046f80
Franziska Sauerwein <franzi@codurance.com>
2015-09-03 14:43:50 +0100

    test all player numbers from 0 to 5 parameterized

--- java/src/test/java/uglytrivia/GameShould.java
+++ java/src/test/java/uglytrivia/GameShould.java
@@ -1,16 +1,22 @@
 package uglytrivia;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import com.adaptionsoft.games.trivia.Printer;
 import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(Parameterized.class)
 public class GameShould {
+    private final int numberOfPlayers;
     private Game game;
     private List<String> printedLines;
     private Printer printer = new Printer() {
@@ -20,6 +26,17 @@ public class GameShould {
         }
     };
 
+    @Parameters(name = "{index}: create {0} players")
+    public static Iterable<Object[]> numbers_from_0_to_5() {
+        return Arrays.asList(new Object[][]{
+                {0}, {1}, {2}, {3}, {4}, {5}
+        });
+    }
+
+    public GameShould(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+    }
+
     @Before
     public void initialise() {
         printedLines = new ArrayList<String>();
@@ -28,14 +45,10 @@ public class GameShould {
 
     @Test
     public void
-    has_no_players_initially() {
-        assertThat(game.numberOfPlayers(), is(0));
-    }
-
-    @Test
-    public void
-    has_one_player_after_adding() {
-        game.add("player1");
-        assertThat(game.numberOfPlayers(), is(1));
+    counts_added_Players() {
+        for (int playerNumber = 0; playerNumber < numberOfPlayers; playerNumber++) {
+            game.add("player" + playerNumber);
+        }
+        assertThat(game.numberOfPlayers(), is(numberOfPlayers));
     }
 }

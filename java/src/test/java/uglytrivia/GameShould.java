e932b8ec014bb6466a71d88082ff3af62e28e894
Franziska Sauerwein <franzi@codurance.com>
2015-09-06 22:28:18 +0100

    add test for player getting out of the penalty box

--- java/src/test/java/uglytrivia/GameShould.java
+++ java/src/test/java/uglytrivia/GameShould.java
@@ -237,6 +237,33 @@ public class GameShould {
         ));
     }
 
+    @Test
+    public void
+    executes_no_move_if_in_penalty_box_and_odd_roll() {
+        addPlayers(2);
+
+        game.movePlayerInPenaltyBoxAfterWrongAnswerAndMoveOnToNextPlayer();
+        game.movePlayerInPenaltyBoxAfterWrongAnswerAndMoveOnToNextPlayer();
+        game.executeMove(3);
+
+        assertThat(printedLines, contains(
+                "player1 was added",
+                "They are player number 1",
+                "player2 was added",
+                "They are player number 2",
+                "Question was incorrectly answered",
+                "player1 was sent to the penalty box",
+                "Question was incorrectly answered",
+                "player2 was sent to the penalty box",
+                "player1 is the current player",
+                "They have rolled a 3",
+                "player1 is getting out of the penalty box",
+                "player1's new location is 3",
+                "The category is Rock",
+                "Rock Question 0"
+        ));
+    }
+
     private void addPlayers(int numberOfPlayersToAdd) {
         for (int playerNumber = 0; playerNumber < numberOfPlayersToAdd; playerNumber++) {
             game.add("player" + (playerNumber + 1));

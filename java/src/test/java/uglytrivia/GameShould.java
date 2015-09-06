c98eb37d04672a25c1aa3413f1713fbb0e89535f
Franziska Sauerwein <franzi@codurance.com>
2015-09-06 14:11:20 +0100

    refactor tests to be more concise

--- java/src/test/java/uglytrivia/GameShould.java
+++ java/src/test/java/uglytrivia/GameShould.java
@@ -1,22 +1,24 @@
 package uglytrivia;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import com.adaptionsoft.games.trivia.Printer;
 import com.adaptionsoft.games.uglytrivia.Game;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 
-@RunWith(Parameterized.class)
+@RunWith(JUnitParamsRunner.class)
 public class GameShould {
-    private final int numberOfPlayers;
     private Game game;
     private List<String> printedLines;
     private Printer printer = new Printer() {
@@ -26,20 +28,12 @@ public class GameShould {
         }
     };
 
-    @Parameters(name = "{index}: create {0} players")
-    public static Iterable<Object[]> numbers_from_0_to_5() {
-        return Arrays.asList(new Object[][]{
-                {0}, {1}, {2}, {3}, {4}, {5}
-        });
-    }
-
-    public GameShould(int numberOfPlayers) {
-        this.numberOfPlayers = numberOfPlayers;
-    }
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void initialise() {
-        printedLines = new ArrayList<String>();
+        printedLines = new ArrayList<>();
         game = new Game(printer);
     }
 
@@ -50,22 +44,37 @@ public class GameShould {
     }
 
     @Test
+    @Parameters({"0", "1", "2", "3", "4", "5"})
     public void
-    counts_added_Players() {
+    counts_added_Players(Integer numberOfPlayers) {
         addPlayers(numberOfPlayers);
 
         assertThat(game.numberOfPlayers(), is(numberOfPlayers));
+    }
+
+    @Test
+    public void
+    throws_an_exception_for_more_than_5_players() {
+        addPlayers(5);
+
+        thrown.expect(ArrayIndexOutOfBoundsException.class);
+
+        game.add("oneToMuch");
+    }
+
+
+    @Test
+    @Parameters({"0,false", "1,false", "2,true", "3,true", "4,true"})
+    public void
+    be_playable_with_at_least_two_players(Integer numberOfPlayers, boolean isPlayable) {
+        addPlayers(numberOfPlayers);
+
+        assertThat(game.isPlayable(), is(isPlayable));
     }
 
     private void addPlayers(int numberOfPlayersToAdd) {
         for (int playerNumber = 0; playerNumber < numberOfPlayersToAdd; playerNumber++) {
             game.add("player" + playerNumber);
         }
-    }
-
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void
-    throws_an_exception_for_more_than_5_players() {
-        addPlayers(6);
     }
 }
